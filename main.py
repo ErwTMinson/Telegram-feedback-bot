321d895534bedc707c8fa9204a4295db9f56f027
ErwTMinson <110468000+ErwTMinson@users.noreply.github.com>
2022-08-19 14:38:41 +0300

    Fixed bugs and added command "self_id".

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=constants.TOKEN)
 dp = Dispatcher(bot)
 
+@dp.message_handler(commands = ["self_id"])
+async def get_id(message: types.Message):
+    await message.reply(f"Your id: {message.from_user.id}")
+
 @dp.message_handler(commands = ['start', 'help'])
 async def send_start(message: types.Message):
     await message.reply(constants.HELP)
@@ -17,12 +21,16 @@ async def send_start(message: types.Message):
 async def feedback(message: types.Message):
 
     ask_user = -1
-    try:
-        ask_user = message.reply_to_message.forward_from.id
-    except AttributeError:
-        await message.reply("You can\'t send message to feedback.\nGo to settings, in page \"Privacy and Security\", \"forwarded messages\" and set to \"Everybody\"")
+
+    if not message.reply_to_message:
+
+        try:
+            ask_user = message.reply_to_message.forward_from.id
+        except AttributeError:
+            await message.reply("You can\'t send message to feedback.\nGo to settings, in page \"Privacy and Security\", \"forwarded messages\" and set to \"Everybody\"")
 
     if message.reply_to_message:
+
         if ask_user != -1:
             await bot.send_message(text = message.text, chat_id = message.reply_to_message.forward_from.id)
         else:
